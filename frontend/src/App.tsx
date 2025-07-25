aec6b473b3a87b9adbcde60e2506ef68786fe8b1
Larry Banks <larrykbanks21@gmail.com>
2025-07-25 14:12:30 -0400

    frontened

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Link from '@mui/joy/Link'
+import Box from '@mui/joy/Box'
+
+function App() {
+  return (
+    <>
+        <Link href="#redirect">
+          Sign in with google
+        </Link>
+    </>
+  )
+}
+
+export default App
